a2f89d8445c972609ddd0ba1198da1c28645329c
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-01 23:43:22 -0500

    404 page

---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Segment, Header } from 'semantic-ui-react';
+
+const NoMatch = () =>
+    (
+    <Segment>
+        <Header>
+            404 Not Found
+        </Header>
+    </Segment>
+    )
+
+export default NoMatch;
