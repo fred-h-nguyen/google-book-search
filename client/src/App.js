4399e0175eba916251d847bfa191a220d9e3b7bb
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-01 23:43:08 -0500

    added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Title from './components/TitleField/Title'
+import SearchForm from './components/SearchFormField/SearchForm'
+import Saved from './components/Saved/Saved'
+import NoMatch from './components/NoMatch'
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <Router>
         <Navbar />
         <Title />
-      </>
+        <Switch>
+          <Route exact path = '/' component={SearchForm}/>
+          <Route exact path = '/' component={Saved}/>
+          <Route component={NoMatch}/>
+        </Switch>
+      </Router>
     );
   }
 }
