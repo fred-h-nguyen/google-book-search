6a408a3ad928988968ea93380a72de62e4c3460f
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-01 23:46:31 -0500

    react axios calls

---
+++ client/src/utils/API.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export default {
+    getBooks: (query) => {
+        return axios.get('/api/google', { params: { q: `title:${query}` } });
+    },
+    getSavedBooks: () => {
+        return axios.get('/api/books');
+    },
+    deleteBook: (id) => {
+        return axios.delete(`/api/books/${id}`)
+    },
+    saveBook: (bookData) => {
+        return axios.post('/api/books', bookData);
+    }
+};
